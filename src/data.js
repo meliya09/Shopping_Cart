dd7d2e899fdaa385b117d58746fd46c662b67de5
meliya09 <meliya090599@gmail.com>
2023-03-04 22:03:08 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const products = [
+  {
+    id: 1,
+    title: "Blue Denim Shirt",
+    price: 17.99,
+    category: "men's clothing",
+    image: "/images/baju.jpg",
+    rating: {
+      rate: 3.9,
+      count: 120,
+    },
+  },
+  {
+    id: 2,
+    title: "Red Hoodie",
+    price: 35.99,
+    category: "men's clothing",
+    image:"/images/hoodie.jpg",
+    rating: {
+      rate: 4.1,
+      count: 259,
+    },
+  },
+  {
+    id: 3,
+    title: "Oversize T-shirt",
+    price: 25.90,
+    category: "men's clothing",
+    image: "/images/kaospria.jpg",
+    rating: {
+      rate: 4.7,
+      count: 500,
+    },
+  },
+  {
+    id: 4,
+    title: "Demin Shirt",
+    price: 15.99,
+    category: "men's clothing",
+    image: "/images/kemeja2.jpg",
+    rating: {
+      rate: 2.1,
+      count: 430,
+    },
+  },
+];
